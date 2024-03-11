dfc8ee2b9def31a7d85768b8eff8ff009d59c8b3
Max Popper <maxpopper@wustl.edu>
2024-03-11 14:56:30 -0400

    completed queries

--- EmployeeSQL/Starter_Code/queries.sql
+++ EmployeeSQL/Starter_Code/queries.sql
@@ -85,3 +85,79 @@ REFERENCES titles (title_id);
 
 ALTER TABLE salaries ADD CONSTRAINT fk_salaries_emp_no FOREIGN KEY(emp_no)
 REFERENCES employees (emp_no);
+
+
+-- List the emp_no, last_name, first_name, sex, and salary of each employee 
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+FROM employees as e
+INNER JOIN salaries as s ON
+s.emp_no = e.emp_no;
+
+-- List the first_name, last_name, and hire_date for the employees who were hired in 1986
+SELECT first_name, last_name, hire_date FROM employees
+WHERE EXTRACT(YEAR FROM hire_date) = 1986;
+
+-- List the manager of each dept along with their dept_no, dept_name, emp_no, last_name, and first_name
+SELECT dm.dept_no,
+		dm.emp_no,
+		d.dept_name,
+		e.last_name,
+		e.first_name
+FROM dept_manager as dm
+INNER JOIN departments as d ON
+d.dept_no = dm.dept_no
+INNER JOIN employees as e ON
+e.emp_no = dm.emp_no;
+
+-- List the dept_no for each employee along with that employee’s emp_no, last_name, first_name, and dept_name
+SELECT de.dept_no, 
+		de.emp_no, 
+		e.last_name,
+		e.first_name,
+		d.dept_name
+FROM dept_emp as de
+INNER JOIN employees as e ON
+e.emp_no = de.emp_no
+INNER JOIN departments as d ON
+d.dept_no = de.dept_no;
+
+-- List first_name, last_name, and sex of each employee whose first name is Hercules 
+-- and whose last name begins with the letter B
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules'
+GROUP BY first_name, last_name, sex
+HAVING last_name LIKE 'B%'
+
+-- List each employee in the Sales department, including their emp_no, last_name, and first_name
+SELECT d.dept_name, 
+		de.emp_no, 
+		e.last_name, 
+		e.first_name
+FROM dept_emp as de
+INNER JOIN departments as d ON
+d.dept_no = de.dept_no
+INNER JOIN employees as e ON
+e.emp_no = de.emp_no
+WHERE d.dept_name = 'Sales';
+
+-- List each employee in the Sales and Development departments 
+-- including their emp_no, last_name, first_name, and dept_name
+SELECT de.emp_no, 
+		e.last_name, 
+		e.first_name,
+		d.dept_name
+FROM dept_emp as de
+INNER JOIN departments as d ON
+d.dept_no = de.dept_no
+INNER JOIN employees as e ON
+e.emp_no = de.emp_no
+WHERE d.dept_name IN ('Sales', 'Development');
+
+-- List the frequency counts, in descending order, of all the employee last names 
+-- (that is, how many employees share each last name)
+
+SELECT last_name, COUNT(last_name) as "last_name count"
+FROM employees
+GROUP BY last_name
+ORDER BY "last_name count" DESC
